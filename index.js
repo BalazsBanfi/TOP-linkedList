514395ed1e2711358792d45dff30bd0f724aab91
BalazsBanfi <banfibalazs81@gmail.com>
2023-10-21 19:54:05 +0200

    add append function

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-function Node(value = null, nextNode = null) {
-    return {
-        value = value,
-        nextNode = nextNode
+const oneNode = (value = null, nextNode = null) => {
+  return {
+    value,
+    nextNode,
+  };
+};
+
+const linkedList = () => {
+  let head = null;
+
+  //append new Node to the end of the list
+  const append = (value) => {
+    //create new node
+    const newNode = oneNode(value);
+    // if empty list, the new node will be the head
+    // else the new node goes to the end of the list
+    if (head === null) {
+      head = newNode;
+    } else {
+      let tail = head;
+      while (tail.nextNode !== null) {
+        tail = tail.nextNode;
+      }
+
+      tail.nextNode = newNode;
     }
-}
 
-function linkedList() {
-    return {
-        head = null,
+    const toString = () => {
+      console.log("aa");
+    };
+  };
+  return {
+    append,
+    head,
+    toString,
+  };
+};
 
-        //append new Node to the end of the list
-        append(v) {
-
-            //create new node
-            let newNode = Node(v);
-
-            // if empty list, the new node will be the head
-            // else the new node goes to the end of the list 
-            if (this.head === null) {
-                this.head = newNode;
-            } else {
-                let tail = this.head;
-                while (tail.next !== null) {
-                    tail = tail.next;
-                }
-                tail.next = newNode;
-            }
-
-        }
-
-        toString() {
-            console.log('The list is: ', this);
-        }
-    }
-}
-
-
-
-
-let list = LinkedList()
-linkedList.append(8);
-linkedList.append(10);
-linkedList.toString()
+let list = linkedList();
+console.log(list);
+list.append(8);
+list.append(10);
+list.toString();
